fc2aebf59bcbb17d79a5c75d900d8aef912345d2
Victor Sinitsa <Victor.sinitca@gmail.com>
2021-08-12 22:07:54 +0300

    3.23

--- Components/ThreeInLine/FreeJs2Ddesk/FreeJs2DDesk.tsx
+++ Components/ThreeInLine/FreeJs2Ddesk/FreeJs2DDesk.tsx
@@ -28,106 +28,107 @@ const sw5 = new TextureLoader().load(require('../../../assets/img/G6.png'));
 const sw6 = new TextureLoader().load(require('../../../assets/img/G7.png'));
 const sw7 = new TextureLoader().load(require('../../../assets/img/G8.png'));
 const bw8 = new TextureLoader().load(require('../../../assets/img/G9.png'));
-const radius= 20
+const radius = 20
 
 const shape = new Shape();
 const x = -2.5;
 const y = -5;
-const V=1
-shape.moveTo(x + 2.5*V, y + 2.5*V);
-shape.bezierCurveTo(x + 2.5*V, y + 2.5*V, x + 2*V, y*V, x*V, y*V);
-shape.bezierCurveTo(x - 3*V, y*V, x - 3*V, y + 3.5*V, x - 3*V, y + 3.5*V);
-shape.bezierCurveTo(x - 3*V, y + 5.5*V, x - 1.5*V, y + 7.7*V, x + 2.5*V, y + 9.5*V);
-shape.bezierCurveTo(x + 6*V, y + 7.7*V, x + 8*V, y + 4.5*V, x + 8*V, y + 3.5*V);
-shape.bezierCurveTo(x + 8*V, y + 3.5*V, x + 8*V, y*V, x + 5*V, y*V);
-shape.bezierCurveTo(x + 3.5*V, y*V, x + 2.5*V, y + 2.5*V, x + 2.5*V, y + 2.5*V);
+const V = 1
+shape.moveTo(x + 2.5 * V, y + 2.5 * V);
+shape.bezierCurveTo(x + 2.5 * V, y + 2.5 * V, x + 2 * V, y * V, x * V, y * V);
+shape.bezierCurveTo(x - 3 * V, y * V, x - 3 * V, y + 3.5 * V, x - 3 * V, y + 3.5 * V);
+shape.bezierCurveTo(x - 3 * V, y + 5.5 * V, x - 1.5 * V, y + 7.7 * V, x + 2.5 * V, y + 9.5 * V);
+shape.bezierCurveTo(x + 6 * V, y + 7.7 * V, x + 8 * V, y + 4.5 * V, x + 8 * V, y + 3.5 * V);
+shape.bezierCurveTo(x + 8 * V, y + 3.5 * V, x + 8 * V, y * V, x + 5 * V, y * V);
+shape.bezierCurveTo(x + 3.5 * V, y * V, x + 2.5 * V, y + 2.5 * V, x + 2.5 * V, y + 2.5 * V);
 const extrudeSettings = {
-    steps:   4,
-    depth:  1.6,
+    steps: 4,
+    depth: 1.6,
     bevelEnabled: true,
     bevelThickness: 1,
     bevelSize: 1,
     bevelSegments: 2,
 };
 
-const shapeMass=[
+const shapeMass = [
     {
-        material : new MeshPhongMaterial({
-            color: `#fffb00`,
+        material: new MeshPhongMaterial({
+            color: `#bfec5f`,
             flatShading: true,
-            shininess:150,
-            /*transparent: true,*/
+            /* transparent: true,*/
         }),
-        geometry: new SphereGeometry(radius, 16, 2)
+        geometry: new DodecahedronGeometry(radius, 20)
     },
+
     {
-        material : new MeshPhongMaterial({
+        material: new MeshPhongMaterial({
             color: `#01a512`,
             flatShading: true,
             /*transparent: true,*/
         }),
-        geometry:new SphereGeometry(radius, 16, 5)
+        geometry: new SphereGeometry(radius, 16, 5)
     },
     {
-        material : new MeshPhongMaterial({
+        material: new MeshPhongMaterial({
             color: `#0335b6`,
             flatShading: true,
             /*transparent: true,*/
         }),
-        geometry:new TetrahedronGeometry(radius, 2)
+        geometry: new TetrahedronGeometry(radius, 2)
     },
     {
-        material : new MeshPhongMaterial({
+        material: new MeshPhongMaterial({
             color: `#5b039a`,
             flatShading: true,
             /*transparent: true,*/
         }),
-        geometry:new TorusGeometry(
+        geometry: new TorusGeometry(
             radius, 5,
             20, 40)
     },
     {
-        material : new MeshPhongMaterial({
+        material: new MeshPhongMaterial({
             color: `#ffa900`,
             flatShading: true,
             /*transparent: true,*/
         }),
-        geometry:new TorusKnotGeometry(
-            radius/2, radius/2, 20, 80, 2, 3)
+        geometry: new TorusKnotGeometry(
+            radius / 2, radius / 2, 20, 80, 2, 3)
     },
     {
-        material : new MeshPhongMaterial({
+        material: new MeshPhongMaterial({
             color: `#00a6ff`,
             flatShading: true,
             /*transparent: true,*/
         }),
-        geometry:new BoxGeometry(
+        geometry: new BoxGeometry(
             radius * 1.2, radius * 1.2, radius * 1.2,
             10, 10, 10)
     },
     {
-        material : new MeshPhongMaterial({
+        material: new MeshPhongMaterial({
             color: `#e20a98`,
             flatShading: true,
             /*transparent: true,*/
         }),
-        geometry:new DodecahedronGeometry(radius, 20)
+        geometry: new DodecahedronGeometry(radius, 20)
     },
     {
-        material : new MeshPhongMaterial({
+        material: new MeshPhongMaterial({
             color: `#ff0133`,
             flatShading: true,
             /*transparent: true,*/
         }),
-        geometry:new ExtrudeGeometry(shape, extrudeSettings).scale(3.5,3.5,3.5)
+        geometry: new ExtrudeGeometry(shape, extrudeSettings).scale(3.5, 3.5, 3.5)
     },
     {
-        material : new MeshPhongMaterial({
-            color: `#bfec5f`,
+        material: new MeshPhongMaterial({
+            color: `#fffb00`,
             flatShading: true,
-           /* transparent: true,*/
+            shininess: 150,
+            /*transparent: true,*/
         }),
-        geometry:new DodecahedronGeometry(radius, 20)
+        geometry: new SphereGeometry(radius, 16, 2)
     },
 ]
 
@@ -136,9 +137,14 @@ type FreeJs2DDeskType = {
     deskState: deskStateType
     layoutState: LayoutStateType
 }
+
 type CubesType = Array<Array<{
     sector: SectorGameType,
-    cube: { cell: CellMesh; material: MeshPhongMaterial; geometry:BoxGeometry , rotateSystem: Object3D, vertBonus:BonusMesh}
+    cube: {
+        cell: CellMesh; material: MeshPhongMaterial; geometry: BoxGeometry, headerPoint: Object3D,
+        rotateSystemHoriz: Object3D | null, horizBonus: BonusMesh | null,
+        rotateSystemVert: Object3D | null, vertBonus: BonusMesh | null,
+    }
 }>>
 const FreeJs2DDesk: FC<FreeJs2DDeskType> = ({map, deskState, layoutState}) => {
     let timeout: number;
@@ -158,13 +164,11 @@ const FreeJs2DDesk: FC<FreeJs2DDeskType> = ({map, deskState, layoutState}) => {
         const camera2D = new OrthographicCamera(
             widthGL / -2, widthGL / 2,
             heightGL / 2, heightGL / -2, 1, 1000);
-
+        /*const camera2D = new PerspectiveCamera(45,width/height, 0.1, 5000)*/
         /*const controls = new OrbitControls(camera2D, renderer.domElement);
         controls.target.set(0, 5, 0);*/
 
-       /* controls.update();*/
-
-
+        /* controls.update();*/
 
 
         /*console.log(`gLViewEl.clientHeight: ${gLViewEl.clientHeight} gLViewEl.clientWidth ${gLViewEl.clientWidth}`)
@@ -190,8 +194,6 @@ const FreeJs2DDesk: FC<FreeJs2DDeskType> = ({map, deskState, layoutState}) => {
         scene.add(light.target);
 
 
-
-
         let cubes = Array.from(Array(deskState.x),
             () => new Array(deskState.y)
         ) as CubesType
@@ -204,6 +206,8 @@ const FreeJs2DDesk: FC<FreeJs2DDeskType> = ({map, deskState, layoutState}) => {
             const scaleSelect = new Vector3(1.2, 1.2, 1.2)
             const imgMass = [sw0, sw1, sw2, sw3, sw4, sw5, sw6, sw7, bw8]
 
+            const defVector = new Vector3()
+
             for (let i = 0; i < map.length; i++) {
                 for (let j = 0; j < map[0].length; j++) {
 
@@ -211,7 +215,82 @@ const FreeJs2DDesk: FC<FreeJs2DDeskType> = ({map, deskState, layoutState}) => {
                         cubes[i][j].cube.material.color = shapeMass[map[i][j].date.state].material.color
                         cubes[i][j].cube.geometry.copy(shapeMass[map[i][j].date.state].geometry)
                         cubes[i][j].cube.material.needsUpdate = true;
+                        cubes[i][j].cube.cell.rotation.set( -10, -10, 0 );
+
                     }
+
+                    if (map[i][j].date.bonusSector) {
+                        if (map[i][j].date.bonusSector == 1) {
+                            if (!cubes[i][j].cube.rotateSystemHoriz) {
+                                const rotateSystemHor = new Object3D();
+                                const bonusHor = new BonusMesh(5)
+                                bonusHor.position.x = 30
+                                rotateSystemHor.add(bonusHor)
+                                cubes[i][j].cube.headerPoint.add(rotateSystemHor)
+                                cubes[i][j].cube.rotateSystemHoriz = rotateSystemHor
+                                cubes[i][j].cube.horizBonus = bonusHor
+                            }
+                            if (cubes[i][j].cube.rotateSystemVert) {
+                                cubes[i][j].cube.vertBonus?.removeFromParent()
+                                cubes[i][j].cube.rotateSystemVert?.removeFromParent()
+                                cubes[i][j].cube.vertBonus = null
+                                cubes[i][j].cube.rotateSystemVert = null
+                            }
+
+
+                        }
+                        if (map[i][j].date.bonusSector == 2) {
+                            if (!cubes[i][j].cube.rotateSystemVert) {
+                                const rotateSystemVert = new Object3D();
+                                const bonusVert = new BonusMesh(5)
+                                bonusVert.position.y = 30
+                                rotateSystemVert.add(bonusVert)
+                                cubes[i][j].cube.headerPoint.add(rotateSystemVert)
+                                cubes[i][j].cube.rotateSystemVert = rotateSystemVert
+                                cubes[i][j].cube.vertBonus = bonusVert
+                            }
+                            if (cubes[i][j].cube.rotateSystemHoriz) {
+                                cubes[i][j].cube.horizBonus?.removeFromParent()
+                                cubes[i][j].cube.rotateSystemHoriz?.removeFromParent()
+                                cubes[i][j].cube.horizBonus = null
+                                cubes[i][j].cube.rotateSystemHoriz = null
+                            }
+                        }
+                        if (map[i][j].date.bonusSector == 3) {
+                            if (!cubes[i][j].cube.rotateSystemHoriz) {
+                                const rotateSystemHor = new Object3D();
+                                const bonusHor = new BonusMesh(5)
+                                bonusHor.position.x = 30
+                                rotateSystemHor.add(bonusHor)
+                                cubes[i][j].cube.headerPoint.add(rotateSystemHor)
+                                cubes[i][j].cube.rotateSystemHoriz = rotateSystemHor
+                                cubes[i][j].cube.horizBonus = bonusHor
+                            }
+                            if (!cubes[i][j].cube.rotateSystemVert) {
+                                const rotateSystemVert = new Object3D();
+                                const bonusVert = new BonusMesh(5)
+                                bonusVert.position.y = 30
+                                rotateSystemVert.add(bonusVert)
+                                cubes[i][j].cube.headerPoint.add(rotateSystemVert)
+                                cubes[i][j].cube.rotateSystemVert = rotateSystemVert
+                                cubes[i][j].cube.vertBonus = bonusVert
+                            }
+                        }
+                    } else {
+                        if (cubes[i][j].cube.rotateSystemVert) {
+                            cubes[i][j].cube.vertBonus?.removeFromParent()
+                            cubes[i][j].cube.rotateSystemVert?.removeFromParent()
+                            cubes[i][j].cube.vertBonus = null
+                            cubes[i][j].cube.rotateSystemVert = null
+                        }
+                        if (cubes[i][j].cube.rotateSystemHoriz) {
+                            cubes[i][j].cube.horizBonus?.removeFromParent()
+                            cubes[i][j].cube.rotateSystemHoriz?.removeFromParent()
+                            cubes[i][j].cube.horizBonus = null
+                            cubes[i][j].cube.rotateSystemHoriz = null
+                        }
+                    }
+
 
                     if (map[i][j].sectorState.isSelected) {
                         /*console.log(`isSelected`)*/
@@ -226,7 +305,8 @@ const FreeJs2DDesk: FC<FreeJs2DDeskType> = ({map, deskState, layoutState}) => {
                             const x = animateMove.animateObject.shiftJ * sizeCell
                             const y = -animateMove.animateObject.shiftI * sizeCell
                             const addVector = new Vector3(x, y, 0)
-                            cubes[i][j].cube.cell.position.add(addVector)
+                            /*cubes[i][j].cube.cell.position.add(addVector)*/
+                            cubes[i][j].cube.headerPoint.position.add(addVector)
                         }
                     }
                     cubes[i][j].sector = JSON.parse(JSON.stringify(map[i][j]))
@@ -237,10 +317,22 @@ const FreeJs2DDesk: FC<FreeJs2DDeskType> = ({map, deskState, layoutState}) => {
         function animateShift() {
             for (let i = 0; i < cubes.length; i++) {
                 for (let j = 0; j < cubes[0].length; j++) {
-                    cubes[i][j].cube.rotateSystem.rotateY(0.1)
+                    if (cubes[i][j].cube.rotateSystemHoriz) {
+                        cubes[i][j].cube.rotateSystemHoriz?.rotateY(0.1)
+                    }
+                    if (cubes[i][j].cube.rotateSystemVert) {
+                        cubes[i][j].cube.rotateSystemVert?.rotateX(0.1)
+                    }
+
+                    if(cubes[i][j].sector.date.state === 8){
+                        cubes[i][j].cube.cell.rotateX(0.1)
+                    }
+
+
+
                     /*if (Map.current[i][j].sectorState.animateMove) {*/
                     if (cubes[i][j].sector.sectorState.animateMove) {
-                        const cell = cubes[i][j].cube.cell
+                        const cell = cubes[i][j].cube.headerPoint
                         const sector = cubes[i][j].sector
                         const distance = 0.2
                         /* const sector = Map.current[i][j]*/
@@ -294,7 +386,7 @@ const FreeJs2DDesk: FC<FreeJs2DDeskType> = ({map, deskState, layoutState}) => {
 
 
         addOnScene(sizeCell, widthGL, heightGL, scene, map, cubes)
-        camera2D.position.set(0, 0, 100);
+        camera2D.position.set(0, 0, 700);
         camera2D.lookAt(scene.position);
 
         const render = () => {
@@ -346,30 +438,39 @@ function addOnScene(sizeCell: number, widthGL: number, heightGL: number, scene: 
     function createShape(sector: SectorGameType) {
         const material = new MeshPhongMaterial().copy(shapeMass[sector.date.state].material)
         const geometry = new BoxGeometry().copy(shapeMass[sector.date.state].geometry)
+        const headerPoint = new Object3D();
+        const {x, y, z} = getDefaultPosition(sector, widthGL, heightGL, sizeCell)
+        headerPoint.position.set(x, y, z)
 
         const cell = new CellMesh1(material, geometry)
-        const {x, y, z} = getDefaultPosition(sector, widthGL, heightGL, sizeCell)
-        cell.position.set(x, y, z)
+        /* cell.position.set(x, y, z)*/
         cell.rotateX(-10)
         cell.rotateY(-10)
 
-        const rotateSystem = new Object3D();
-        const bonus = new BonusMesh(5)
-        bonus.position.x=30
-        rotateSystem.position.set(x, y, z)
+        /* const rotateSystem = new Object3D();
+         const bonus = new BonusMesh(5)
+         bonus.position.x=30*/
+        /*rotateSystem.position.set(x, y, z)*/
         /*rotateSystem.rotateX(-10)
         rotateSystem.rotateY(-10)*/
-        rotateSystem.add(bonus)
 
-        scene.add(cell)
-        scene.add(rotateSystem)
+
+        /* headerPoint.add(rotateSystem)
+         rotateSystem.add(bonus)*/
+
+        headerPoint.add(cell)
+
+        scene.add(headerPoint)
 
         return {
+            headerPoint: headerPoint,
             cell: cell,
             material: material,
-            geometry:geometry,
-            rotateSystem:rotateSystem,
-            vertBonus:bonus,
+            geometry: geometry,
+            rotateSystemHoriz: null,
+            horizBonus: null,
+            rotateSystemVert: null,
+            vertBonus: null,
         }
     }
 
@@ -409,9 +510,10 @@ class CellMesh1 extends Mesh {
         );
     }
 }
+
 class BonusMesh extends Mesh {
     constructor(
-        radius:number,
+        radius: number,
     ) {
         super(
             new SphereGeometry(radius, 10, 10),
